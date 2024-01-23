fe2802d80dfd21c6afe6ba380cc743db9ade225c
indusRealEstate <webtech@indusre.ae>
2024-01-23 16:22:19 +0400

    refactor: :zap: added last added feature to edit listing.

--- src/app/pages/edit-listing/edit-listing.component.ts
+++ src/app/pages/edit-listing/edit-listing.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
 import { HttpEvent, HttpEventType } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
@@ -214,6 +215,23 @@ export class EditListingComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  imageDrop(event: CdkDragDrop<any>, type) {
+    console.log(event, type);
+    if (type == "g") {
+      moveItemInArray(
+        this.gallary_imgs,
+        event.previousContainer.data.index,
+        event.container.data.index
+      );
+    } else {
+      moveItemInArray(
+        this.floor_imgs,
+        event.previousContainer.data.index,
+        event.container.data.index
+      );
+    }
+  }
 
   addNewFeature(feature) {
     this.features.push(feature);
